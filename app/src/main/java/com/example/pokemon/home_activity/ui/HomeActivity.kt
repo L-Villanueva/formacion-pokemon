391de1f37d3e8c3d53a6426a44df1c159a868f6b
Luis Villanueva <luisvillanueva97@gmail.com>
2021-03-17 19:57:58 +0100

    Auto stash before merge of "main" and "origin/main"

--- app/src/main/java/com/example/pokemon/home_activity/ui/HomeActivity.kt
+++ app/src/main/java/com/example/pokemon/home_activity/ui/HomeActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.pokemon.R
 import com.example.pokemon.commons.BaseActivity
 import com.example.pokemon.home_activity.ui.home.HomeViewModel
+import com.example.pokemon.ui.home.HomeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeActivity : BaseActivity() {
