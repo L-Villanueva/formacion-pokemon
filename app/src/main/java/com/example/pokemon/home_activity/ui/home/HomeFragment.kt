cdc96556f8653ea511f7564d55c03eed351d250f
Luis Villanueva <luisvillanueva97@gmail.com>
2021-03-17 19:51:20 +0100

    Auto stash before merge of "main" and "origin/main"

--- app/src/main/java/com/example/pokemon/home_activity/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pokemon/home_activity/ui/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.pokemon.ui.home
+package com.example.pokemon.home_activity.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,21 +9,21 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.pokemon.R
+import com.example.pokemon.commons.BaseFragment
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+private val presenter : HomeViewModel by sharedViewModel()
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+    override fun loadObservers() {
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        presenter.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
